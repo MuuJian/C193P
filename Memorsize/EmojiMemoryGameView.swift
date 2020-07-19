8b5f04957216603914f94d849b854f35c358f0ac
MuuYiz <xairzy@gmail.com>
2020-07-20 01:03:32 +0800

    FaceUp

--- Memorsize/EmojiMemoryGameView.swift
+++ Memorsize/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  Memorsize
 //
 //  Created by mumu on 18/7/2020.
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct EmojiMemoryGameView: View {
 	var viewModel: EmojiMemoryGame
 	
     var body: some View {
@@ -53,6 +53,6 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: EmojiMemoryGame())
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
     }
 }
